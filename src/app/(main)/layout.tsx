e03c62e05a0b81d8cef0e5d9dc60ac606ae03d10
Alexandru Bejenari <alexandru.bejenari.falesti@gmail.com>
2024-07-30 13:51:49 +0300

    add whoToFollow component

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -20,7 +20,7 @@ export default async function Layout({
       <div className="flex min-h-screen flex-col">
         <Navbar />
         <div className="mx-auto flex w-full max-w-7xl grow gap-5 p-5">
-          <MenuBar className="lg:px5 sticky top-[5.25rem] hidden h-fit flex-none space-y-3 rounded-2xl bg-card px-3 py-5 shadow-sm sm:block xl:min-w-fit" />
+          <MenuBar className="lg:px5 sticky top-[5.25rem] hidden h-fit flex-none space-y-3 rounded-2xl bg-card px-3 py-5 shadow-sm sm:block xl:min-w-80" />
           {children}
         </div>
         <MenuBar className="sticky bottom-0 flex w-full justify-center gap-5 border-t bg-card sm:hidden" />
